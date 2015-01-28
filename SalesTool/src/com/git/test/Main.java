84ed3376639818e63a5766750b1959589115d78d
Calvin <cjames@bellsouth.net>
2015-01-28 09:20:08 -0600

    this my initial test of git

---
+++ SalesTool/src/com/git/test/Main.java
@@ -0,0 +1,17 @@
+package com.git.test;
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		SalesData data =  new SalesData();
+	}
+	public void displayGreeting() {
+		System.out.println("HELLO HAPPY SALES PEOPLE!");
+		System.out.println("THIS APP SHOW SALES DATA !");
+	}
+
+}
---
+++ SalesTool/src/com/git/test/SalesData.java
@@ -0,0 +1,15 @@
+package com.git.test;
+
+public class SalesData {
+
+	/**
+	 * @param args
+	 */
+	
+	int data[]  = {4,12,12};
+	public  void display() {
+		// TODO Auto-generated method stub
+		System.out.println("TODO!");
+	}
+
+}
